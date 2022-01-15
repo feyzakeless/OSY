c8d1cd1b2f8952cc5eb7309ce8fdb87be132c788
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:40:10 +0300

    Housing controller added

---
+++ OSY.API/Controllers/HousingController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using OSY.Model;
+using OSY.Model.ModelHousing;
+using OSY.Service.HousingServiceLayer;
+
+namespace OSY.API.Controllers
+{
+    [Route("[controller]s")]
+    [ApiController]
+    public class HousingController : Controller
+    {
+        private readonly IHousingService housingService;
+        private readonly IMapper mapper;
+        public HousingController(IHousingService _housingService, IMapper _mapper)
+        {
+            housingService = _housingService;
+            mapper = _mapper;
+        }
+
+
+        // Blok(Bina) Ekleme
+        [HttpPost]
+        public General<HousingViewModel> Insert([FromBody] HousingViewModel newHousing)
+        {
+            General<HousingViewModel> response = new();
+            response = housingService.Insert(newHousing);
+            return response;
+        }
+        
+
+        // Blok(Bina) Listeleme
+        [HttpGet]
+        public General<HousingViewModel> GetList()
+        {
+            return housingService.GetList();
+        }
+
+
+        // Blok(Bina) Guncelleme
+        [HttpPut("{id}")]
+        public General<HousingViewModel> Update([FromBody] HousingViewModel housing, int id)
+        {
+            return housingService.Update(housing, id);
+        }
+
+
+        // Blok(Bina) Silme
+        [HttpDelete("{id}")]
+        public General<HousingViewModel> Delete(int id)
+        {
+            return housingService.Delete(id);
+        }
+    }
+}
