ff366721e5796e4fb3aefdbaebded24acadd79e3
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:43:07 +0300

    Resident model added

---
+++ OSY.Model/ModelResident/ResidentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelResident
+{
+    public class ResidentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string IdentityNo { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PlateNo { get; set; }
+        public int ApartId { get; set; }
+        public string IsAdmin { get; set; }
+    }
+}
