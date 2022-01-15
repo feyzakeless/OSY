ba059d8e85a22fed13c2f6dc6a4c77a212c5ae31
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:49:33 +0300

    Housing service interface added

---
+++ OSY.Service/HousingServiceLayer/IHousingService.cs
@@ -0,0 +1,18 @@
+﻿using OSY.Model;
+using OSY.Model.ModelHousing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Service.HousingServiceLayer
+{
+    public interface IHousingService
+    {
+        public General<HousingViewModel> Insert(HousingViewModel newHousing);
+        public General<HousingViewModel> GetList();
+        public General<HousingViewModel> Update(HousingViewModel housing, int id);
+        public General<HousingViewModel> Delete(int id);
+    }
+}
