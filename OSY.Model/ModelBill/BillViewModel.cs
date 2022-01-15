dbfe03459eb4879b30c7ca057fe1084b05daafc7
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:42:12 +0300

    Apartment bill added

---
+++ OSY.Model/ModelBill/BillViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelBill
+{
+    public class BillViewModel
+    {
+        public int Id { get; set; }
+        public string BillType { get; set; }
+        public decimal Paid { get; set; }
+        public decimal UnPaid { get; set; }
+        public decimal TotalDept { get; set; }
+        public int Iapartment { get; set; }
+    }
+}
