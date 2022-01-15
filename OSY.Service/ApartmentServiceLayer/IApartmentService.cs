907b2ba614ed696fc0b86989335083e63527dd91
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:48:54 +0300

    Aparment service interface added

---
+++ OSY.Service/ApartmentServiceLayer/IApartmentService.cs
@@ -0,0 +1,18 @@
+﻿using OSY.Model;
+using OSY.Model.ModelApartment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Service.ApartmentServiceLayer
+{
+    public interface IApartmentService
+    {
+        public General<ApartmentViewModel> Insert(ApartmentViewModel newApartment);
+        public General<ApartmentViewModel> GetList();
+        public General<ApartmentViewModel> Update(ApartmentViewModel apartment, int id);
+        public General<ApartmentViewModel> Delete(int id);
+    }
+}
