b2331cda7d04ac22a2a035be4004e3254e821c41
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:42:05 +0300

    Apartment model added

---
+++ OSY.Model/ModelApartment/ApartmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelApartment
+{
+    public class ApartmentViewModel
+    {
+        public int Id { get; set; }
+        public int BlokId { get; set; }
+        public int ApartmentNo { get; set; }
+        public string ApartmentType { get; set; }
+        public bool IsFull { get; set; }
+    }
+}
