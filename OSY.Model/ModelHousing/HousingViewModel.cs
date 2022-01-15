3467c9aaac1a3962c2068e2552933d00600148e9
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:42:31 +0300

    Housing model added

---
+++ OSY.Model/ModelHousing/HousingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelHousing
+{
+    public class HousingViewModel
+    {
+        public int Id { get; set; }
+        public string BlokName { get; set; }
+    }
+}
