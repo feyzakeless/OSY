5b7ceed82e9275cbf76494a4797e282d9c3a6bc6
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:51:22 +0300

    Bill service interface added

---
+++ OSY.Service/BillServiceLayer/IBillService.cs
@@ -0,0 +1,18 @@
+﻿using OSY.Model;
+using OSY.Model.ModelBill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Service.BillServiceLayer
+{
+    public interface IBillService
+    {
+        public General<BillViewModel> Insert(BillViewModel newBill);
+        public General<BillViewModel> GetList();
+        public General<BillViewModel> Update(BillViewModel medicine, int id);
+        public General<BillViewModel> Delete(int id);
+    }
+}
