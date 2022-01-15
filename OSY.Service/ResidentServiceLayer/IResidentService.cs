5fd7dbca7d74677ba161431fd6e61c911393c50a
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:50:52 +0300

    Resident service interface added

---
+++ OSY.Service/ResidentServiceLayer/IResidentService.cs
@@ -0,0 +1,20 @@
+﻿using OSY.Model;
+using OSY.Model.ModelLogin;
+using OSY.Model.ModelResident;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Service.ResidentServiceLayer
+{
+    public interface IResidentService
+    {
+        public General<ResidentViewModel> Insert(ResidentViewModel newResident);
+        public General<ResidentViewModel> Login(LoginViewModel loginResident);
+        public General<ResidentViewModel> GetResidents();
+        public General<ResidentViewModel> Update(int id, ResidentViewModel resident);
+        public General<ResidentViewModel> Delete(int id);
+    }
+}
