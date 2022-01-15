cd5c09cd4a92855a9cb53b2c5519bcdac4fc2fc6
feyzakeless <feyzakeles@gmail.com>
2022-01-15 15:43:19 +0300

    General model added

---
+++ OSY.Model/General.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OSY.Model
+{
+    public class General<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Entity { get; set; }
+        public List<T> List { get; set; }
+        public int TotalCount { get; set; }
+        public int SumPageNumber { get; set; }
+        public List<string> ValidationErrorList { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+}
