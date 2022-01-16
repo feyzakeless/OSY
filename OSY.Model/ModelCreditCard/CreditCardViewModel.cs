c6655f00b149585758b20c7ae19c8c34e908ef22
feyzakeless <feyzakeles@gmail.com>
2022-01-17 00:00:43 +0300

    Credit Card Model created

--- OSY.Model/ModelCreditCard/CreditCardViewModel.cs
+++ OSY.Model/ModelCreditCard/CreditCardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,11 @@ namespace OSY.Model.ModelCreditCard
 {
     public class CreditCardViewModel
     {
-        public int BillId { get; set; }
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+
+        public string Id { get; set; }
+        public int ApartmentId { get; set; }
         public long CreditCardNumber { get; set; }
         public int CVV{ get; set; }
     }
