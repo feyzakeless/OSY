ea58c0665268bbd72fd0d10e85ec9678bc6eb6a5
feyzakeless <feyzakeles@gmail.com>
2022-01-16 12:12:11 +0300

    mail method added to interface

--- OSY.Service/Job/IEmailOperations.cs
+++ OSY.Service/Job/IEmailOperations.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OSY.Service.Job
+﻿namespace OSY.Service.Job
 {
     public interface IEmailOperations
     {
