df3969eedbf9e7a5e0b094de370f3fd58d91f32e
feyzakeless <feyzakeles@gmail.com>
2022-01-17 00:01:48 +0300

    Mongodb config file added

---
+++ OSY.Service/PaymentDBConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Service
+{
+    public class PaymentDBConfig
+    {
+        public string Database_Name { get; set; }
+        public string Payments_Collection_Name { get; set; }
+        public string Connection_String { get; set; }
+
+    }
+}
