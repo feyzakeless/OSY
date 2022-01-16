11cea2dedc8089427838292afc9c40656c5d9712
feyzakeless <feyzakeles@gmail.com>
2022-01-16 17:51:17 +0300

    Model has been created for invoice assignment

---
+++ OSY.Model/ModelBill/AssignBillViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelBill
+{
+    public class AssignBillViewModel
+    {
+        public string BillType { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime Idate { get; set; }
+    }
+}
