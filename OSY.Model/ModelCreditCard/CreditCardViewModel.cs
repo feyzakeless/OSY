6caca8d9d97000bfe8630f076aa6d2eb6885f451
feyzakeless <feyzakeles@gmail.com>
2022-01-16 21:16:25 +0300

    Card model created

---
+++ OSY.Model/ModelCreditCard/CreditCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelCreditCard
+{
+    public class CreditCardViewModel
+    {
+        public int BillId { get; set; }
+        public long CreditCardNumber { get; set; }
+        public int CVV{ get; set; }
+    }
+}
