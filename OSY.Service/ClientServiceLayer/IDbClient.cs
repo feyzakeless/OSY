215621790d6b4ea438f038f730dcfd85a482ac3b
feyzakeless <feyzakeles@gmail.com>
2022-01-17 00:01:33 +0300

    DbClient Interface added

---
+++ OSY.Service/ClientServiceLayer/IDbClient.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using OSY.Model.ModelCreditCard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Service.ClientServiceLayer
+{
+    public interface IDbClient
+    {
+        IMongoCollection<CreditCardViewModel> GetCardsCollection();
+    }
+}
