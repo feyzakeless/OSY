ead46ad2b25279c9040f4cb67d0dbe0dc30b1577
feyzakeless <feyzakeles@gmail.com>
2022-01-19 01:54:05 +0300

    Password field removed

--- OSY.Model/ModelResident/ResidentViewModel.cs
+++ OSY.Model/ModelResident/ResidentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,31 @@ namespace OSY.Model.ModelResident
     public class ResidentViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Alanı Boş Olamaz !")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyisim Alanı Boş Olamaz !")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Email Alanı Boş Olamaz !")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Parola Alanı Boş Olamaz !")]
+        [StringLength(8, ErrorMessage = "Parola 8 karakter olmalıdır.")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "TC Alanı Boş Olamaz !")]
         public string IdentityNo { get; set; }
+
+        [Required(ErrorMessage = "Telefon Alanı Boş Olamaz !")]
         public string PhoneNumber { get; set; }
         public string PlateNo { get; set; }
+
+        [Required(ErrorMessage = "Daire Alanı Boş Olamaz !")]
         public int ApartId { get; set; }
+
+        [Required(ErrorMessage = "Yetki Alanı Boş Olamaz !")]
         public string IsAdmin { get; set; }
         public bool IsSendWelcomeMail { get; set; }
     }
