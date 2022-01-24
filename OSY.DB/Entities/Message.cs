aefc19a456625821e45436341e4eeb814117a5e5
feyzakeless <feyzakeles@gmail.com>
2022-01-25 01:29:24 +0300

    Message table added

---
+++ OSY.DB/Entities/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace OSY.DB.Entities
+{
+    public partial class Message
+    {
+        public int Id { get; set; }
+        public string MessageText { get; set; }
+        public int RecevierId { get; set; }
+        public int SenderId { get; set; }
+        public DateTime Idate { get; set; }
+        public bool Seen { get; set; }
+
+        public virtual Resident Recevier { get; set; }
+        public virtual Resident Sender { get; set; }
+    }
+}
