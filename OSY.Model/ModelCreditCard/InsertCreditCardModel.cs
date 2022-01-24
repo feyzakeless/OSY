650da3f543f08be75688d00bcbc4446d428e1e89
feyzakeless <feyzakeles@gmail.com>
2022-01-25 01:33:56 +0300

    Credit card model added for insert operation

---
+++ OSY.Model/ModelCreditCard/InsertCreditCardModel.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSY.Model.ModelCreditCard
+{
+    public class InsertCreditCardModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+
+        public string Id { get; set; }
+
+        public int ApartmentId { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public string CVC { get; set; }
+
+        public string cardDate { get; set; }
+    }
+}
