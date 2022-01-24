cd6c9a9e0ba30e3095b7fb0302d0d6359f01ef48
feyzakeless <feyzakeles@gmail.com>
2022-01-25 01:32:24 +0300

    Credit cart database created for mongoDB

---
+++ OSY.DB/MongoDB/MongoEntities/CreditCard.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OSY.DB.MongoDB.MongoEntities
+{
+    public class CreditCard
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("apartmentId")]
+        public int ApartmentId { get; set; }
+
+        [BsonElement("creditCardNumber")]
+        public string CreditCardNumber { get; set; }
+
+        [BsonElement("cvc")]
+        public string CVC { get; set; }
+
+        [BsonElement("cardDate")]
+        public string cardDate { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+
+    }
+}
